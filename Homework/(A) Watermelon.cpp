01f607f7705eb0daa7cf2aecb6b5863e02fb04da
Fabián Jaña <43253032+fabianjana@users.noreply.github.com>
2018-09-21 03:56:37 -0400

    Create (A) Watermelon.cpp
    need to edit format

---
+++ Homework/(A) Watermelon.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+2
+
+using namespace std;
+
+3
+
+​
+
+4
+
+int main () {
+
+5
+
+​
+
+6
+
+    int w;
+
+7
+
+    cin >> w;
+
+8
+
+    
+
+9
+
+    if (w % 2 == 0 && w != 2) {
+
+10
+
+    cout << "YES";
+
+11
+
+    }
+
+12
+
+    else {
+
+13
+
+    cout << "NO";
+
+14
+
+    }
+
+15
+
+​
+
+16
+
+return 0;
+
+17
+
+}
